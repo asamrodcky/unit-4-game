6dd5bfd35de0313da561ce908b4b8b40b8f88139
samrod826 <samrodriguez826@gmail.com>
2019-10-05 23:56:52 -0400

    most js logic down

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,41 +11,61 @@ var lose = 0;
 // Variable for desired value
 var goal = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
 
+function reset(){
+    crystal1 = Math.floor(Math.random()*13);
+    crystal2 = Math.floor(Math.random()*13);
+    crystal3 = Math.floor(Math.random()*13);
+    crystal4 = Math.floor(Math.random()*13);
+    goal = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+    score = 0;
+    $("#goalValue").text(goal)
+}
+
 //Checks
 console.log(crystal1);
 console.log(crystal2);
 console.log(crystal3);
 console.log(crystal4);
-console.log(goal);
+console.log("goal: "+ goal);
+
+$("#goalValue").text(goal)
 
 if(score < goal){
 $('body').on('click','#crystal1',function(){
     score += crystal1
+    $("#scoreValue").text(score)
     console.log("score: " + score)
 })
 
 $('body').on('click','#crystal2',function(){
     score += crystal2
+    $("#scoreValue").text(score)
     console.log("score: " + score)
 })
 
 $('body').on('click','#crystal3',function(){
     score += crystal3
+    $("#scoreValue").text(score)
     console.log("score: " + score)
 })
 
 $('body').on('click','#crystal4',function(){
     score += crystal4
+    $("#scoreValue").text(score)
     console.log("score: " + score)
 })
 }
-else if (score ===goal){
-    win++
-    alert("YOU WON!")
+else if (score === goal){
+    win++;
+    $("#winLoss").text(win+lose);
+    alert("YOU WON!");
+    reset();
 }
-else{
-    alert("You lose :/")
-    lose++
+else if (score > goal){
+    alert("You lose :/");
+    lose++;
+    $("#winLoss").text(win+lose);
+    reset();
 }
 
 })
